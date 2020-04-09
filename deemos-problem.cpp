e0efc8a51458a70323dcd54e057db223ed945139
Dragoljub Djuric <djuric2103@gmail.com>
2020-04-09 03:29:35 +0200

    Update deemos-problem.cpp

--- deemos-problem.cpp
+++ deemos-problem.cpp
@@ -2,24 +2,23 @@
 using namespace std;
 
 void reduce(vector<int>& diff){
-    const int m = diff.size();
+    const int n = diff.size();
     int min = 1 << 20;
-    for(int i = 0; i< m; i++) {
-        if(diff[i] == 0){min = 0; break;}
+    for(int i = 0; i < n; i++) {
+        if(diff[i] == 0) return;
         if (diff[i] < min) min = diff[i];
     }
-    if(min != 0) for(int i = 0; i < m; i++) diff[i] -= min;
+    if(min != 0) for(int i = 0; i < n; i++) diff[i] -= min;
 }
 
-int countSubarrays(vector<int> arr, int m) {
+int countSubarrays(vector<int>& arr, const int m) {
     vector<int> diff(m, 0);
     map<vector<int>, int> map;
     map[diff]++;
     int result = 0;
     const int n = arr.size();
     for (int i = 0; i < n; i++) {
-        diff[arr[i] - 1]++;
-        if(diff[arr[i] - 1] == 1) reduce(diff);
+        if(++diff[arr[i]] == 1) reduce(diff);
         result += map[diff]++;
     }
     return result;
@@ -30,11 +29,11 @@ int main() {
     cin.tie(NULL);
     int n, m;
     cin >> n>> m;
-    vector<int> arr;
+    vector<int> arr(n);
     int t;
     for(int i = 0; i < n; i++){
         cin>>t;
-        arr.push_back(t);
+        arr[i] = t - 1;
     }
     cout << countSubarrays(arr, m) << "\n";
     return 0;
