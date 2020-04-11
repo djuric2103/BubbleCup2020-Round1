ef439af6bed305a3003ce2a678d7a53f8f1cd579
Dragoljub Djuric <djuric2103@gmail.com>
2020-04-12 01:46:08 +0200

    Update Zig-zag.cpp

--- Zig-zag.cpp
+++ Zig-zag.cpp
@@ -1,21 +1,16 @@
 #include<bits/stdc++.h>
-
-using namespace std;
-
+ 
 int main(){
     long long t, a, b;
-    cin >> t;
+    scanf("%lld", &t);
     while(t--){
-        cin >> a >> b;
-        if(a == b) cout << "-1\n";
-        else if(a > b) cout << "1\n";
-        else{
-            if(a %(b - a) == 0){
-                cout << (2*(a/(b- a)) + 1) << '\n';
-            }else{
-                cout << (2*(a/(b - a) + 1)) << '\n';
-            }
-        }
+        scanf("%lld%lld", &a, &b);
+        if(a == b) printf("-1\n");
+        else if(a > b) printf("1\n");
+        else 
+        	if(a %(b - a) == 0) printf("%lld\n", 2*(a/(b- a)) + 1);
+            else printf("%lld\n", 2*(a/(b - a) + 1));
+        
      }
      return 0;
 }
