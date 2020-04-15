dd49852e9c03954df4ef2d78ff22d717e4a9ce1e
Dragoljub Djuric <djuric2103@gmail.com>
2020-04-15 21:07:08 +0200

    Update ludic_numbers.cpp

--- ludic_numbers.cpp
+++ ludic_numbers.cpp
@@ -1,17 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
+#include <bits/stdc++.h>
 #define greatest 43956562
 #define limit 2275147
 #define logn 25
+#define lastRem 2142957
 using namespace std;
 const int maxn = greatest + 2;
-vector<int> ludic(maxn / 64 + 2), offset(limit), sol(limit), BIT(maxn);
+int ludic[maxn / 64 + 10];
+int offset[limit];
 const int BIT_SIZE = maxn - 1;
-const int sol_size = limit;
-int k = limit, previous = greatest, prev_sol = sol_size;
+int BIT[maxn];
+int sol[limit];
+int k = limit, sol_size = limit, previous = greatest, prev_sol = sol_size;
 unordered_map<int, int> a;
+
+inline void update(int x, int v) {
+    while (x < BIT_SIZE) BIT[x] += v, x += x & -x;
+}
 
 inline int sum(int x) {
     int ret = 0;
@@ -19,47 +23,46 @@ inline int sum(int x) {
     return ret;
 }
 
-void bitWiseSieve(const int n) {
-    for (int i = 1; i <= n; ++i){
-        BIT[i] = i & -i;
-        if(i%2 == 0) BIT[i] /= 2;
-    }
-    for (int i = 0; i <= logn; ++i) ++BIT[1 << i];
+inline int query(int x) {
+    int sum = 0, ret = 0;
+    for (int i = logn; i >= 0; --i)
+        if (ret + (1 << i) < BIT_SIZE && sum + BIT[ret + (1 << i)] < x)
+            sum += BIT[ret + (1 << i)],
+                    ret += 1 << i;
+    return ++ret;
+}
+
+void bitWiseSieve(int n){
+    for (int i = 1; i <= n; ++i) BIT[i] = i & -i;
+    for (int i = 2; i <= n; i += 2) BIT[i] /= 2;
+    for (int i = 0; i <= logn; ++i) BIT[1 << i]++;
     sol[0] = 1, sol[1] = 2, offset[0] = 0, offset[1] = 0;
-    int curr = 2,left = sum(n), i;
-    for (i = 3; i <= 2142957; i += 2) {
-        if (!(ludic[i / 64] & (1 << ((i >> 1) & 31)))) {
+    int sum_n = -1, curr = 2, left = sum(n);
+
+    for (int i = 3; i <= n; i += 2) {
+        if (!(ludic[i/64] & (1 << ((i >> 1) & 31)))) {
             ++curr;
             sol[curr - 1] = i;
-            int s = curr;
-            for (int j = curr + i; j < left + 1; j += i - 1) {
-                int sums = 0, k = 0;
-                for (int m = logn; m >= 0; --m) {
-                    if (k + (1 << m) < BIT_SIZE && sums + BIT[k + (1 << m)] < j)
-                        sums += BIT[k + (1 << m)],
-                                k += 1 << m;
+            if(i <= lastRem) {
+                int s = curr;
+                for (int j = curr + i; j < left + 1; j += i-1) {
+                    int k = query(j);
+                    if (k >= n + 1) break;
+                    ludic[k/64] |= (1 << ((k >> 1) & 31));
+                    --left;
+                    s += (i - 1);
+                    update(k, -1);
                 }
-                ++k;
-                if (k >= n) break;
-                ludic[k / 64] |= (1 << ((k >> 1) & 31));
-                --left;
-                s += (i - 1);
-                while (k < BIT_SIZE) BIT[k] -= 1, k += k & -k;
+                offset[curr - 1] = left - s;
+                continue;
             }
-            offset[curr - 1] = left - s;
-        }
-    }
-    int sum_n = sum(n);
-    for (; i <= n; i += 2) {
-        if (!(ludic[i / 64] & (1 << ((i >> 1) & 31)))) {
-            curr++;
-            sol[curr - 1] = i;
+            if(sum_n == -1) sum_n = sum(n);
             offset[curr - 1] = sum_n - curr;
         }
     }
 }
 
-inline int binary(const int q) {
+inline int binary(int q){
     int low = 0, high = sol_size - 1, mid, ans = -1;
     while (low <= high) {
         mid = (low + high) / 2;
@@ -69,9 +72,7 @@ inline int binary(const int q) {
     return ans;
 }
 
-
-
-inline int return_num(const int n, const vector<int>& sol1) {
+int search_incr(const int n) {
     if (a.find(n) != a.end()) {
         return a.find(n)->second;
     }
@@ -79,13 +80,13 @@ inline int return_num(const int n, const vector<int>& sol1) {
 
     int toRem;
     for (int i = 1; i < k; ++i) {
-        toRem = (offset[i] + additional) / sol1[i];
-        offset[i] = (offset[i]+additional) % sol1[i];
+        toRem = (offset[i] + additional) / sol[i];
+        offset[i] = (offset[i]+additional) % sol[i];
         additional -= toRem;
         if(additional == 0) break;
     }
 
-    const int res = additional + prev_sol;
+    int res = additional + prev_sol;
     prev_sol = res;
     previous = n;
     if (n % 2 == 1) {
@@ -94,7 +95,7 @@ inline int return_num(const int n, const vector<int>& sol1) {
     return res;
 }
 
-inline int return_num1(const int n, const vector<int>& sol1) {
+int search_dec(const int n) {
     if (a.find(n) != a.end()) {
         return a.find(n)->second;
     }
@@ -102,46 +103,48 @@ inline int return_num1(const int n, const vector<int>& sol1) {
     int cout = 0;
     int toRem;
     for (int i = 1; i < k; ++i) {
-        toRem = (offset[i] + additional) / sol1[i];
+        toRem = (offset[i] + additional) / sol[i];
         cout = toRem == 0 ? cout + 1 : 0;
         additional -= toRem;
     }
     k -= cout;
-    const int res = additional + sol_size;
+    int res = additional + sol_size;
     if (n % 2 == 0) {
         a.insert({n - 1, res});
     }
     return res;
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
+
+int main(){
     int t;
-    scanf("%d", &t);
+    scanf("%d",&t);
     bitWiseSieve(greatest);
-    const vector<int> sol1(sol);
+
     vector<int> qu(t);
     vector<int> so(t);
-    for (int i = 0; i < t; ++i) {
+    for(int i = 0; i < t; ++i){
         int temp;
-        scanf("%d", &temp);
+        scanf("%d",&temp);
         qu[i] = temp;
         so[i] = temp;
     }
     sort(so.begin(), so.end());
+
     if(so[t - 1] - so[t/2] < 1e6) {
         for (int i = 0; i < t; ++i) {
-            if (so[i] > greatest)a.insert({so[i], return_num(so[i], sol1)});
+            if (so[i] > greatest)a.insert({so[i], search_incr(so[i])});
             else a.insert({so[i], binary(so[i])});
         }
     }else{
         for (int i = t - 1; i >= 0; --i) {
-            if (so[i] > greatest)a.insert({so[i], return_num1(so[i], sol1)});
+            if (so[i] > greatest)a.insert({so[i], search_dec(so[i])});
             else a.insert({so[i], binary(so[i])});
         }
     }
-    for (int i = 0; i < t; ++i) {
-        printf("%d\n", a.find(qu[i])->second);
+    for(int  i = 0; i < t; ++i){
+        printf("%d\n", a.find(qu[i]) -> second);
     }
+
     return 0;
 }
